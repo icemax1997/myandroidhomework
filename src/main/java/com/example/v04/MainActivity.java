fd36a360f7695410414bbc418ab5415aaf6333ff
icemax1997 <1137574126@qq.com>
2017-05-19 00:00:45 +0800

    校园应用 v0.0.2

---
+++ src/main/java/com/example/v04/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.v04;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    /*
+        *变量定义
+        */
+    private List<View> viewList;
+    private ViewPager pager;
+    private PagerTabStrip tab;
+    private List<String> titleList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        showViewPager();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.title_messages:
+                    pager.setVisibility(View.VISIBLE);
+                    return true;
+                case R.id.title_fun:
+                    pager.setVisibility(View.GONE);
+                    return true;
+                case R.id.title_personal:
+                    pager.setVisibility(View.GONE);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    private void showViewPager() {
+
+            viewList=new ArrayList<View>();
+            View view1=View.inflate(this,R.layout.view1,null);
+            View view2=View.inflate(this,R.layout.view2,null);
+            View view3=View.inflate(this,R.layout.view3,null);
+            viewList.add(view1);
+            viewList.add(view2);
+            viewList.add(view3);
+            titleList=new ArrayList<String>();
+            titleList.add("校园动态");
+            titleList.add("班级动态");
+            titleList.add("其他动态");
+
+            tab= (PagerTabStrip) findViewById(R.id.tab);
+            tab.setBackgroundColor(Color.WHITE);
+            tab.setDrawFullUnderline(false);
+            tab.setTabIndicatorColor(Color.BLUE);
+
+            MyPagerAdapter adapter=new MyPagerAdapter(viewList,titleList);
+            pager= (ViewPager) findViewById(R.id.pager);
+            pager.setAdapter(adapter);
+
+    }
+}
